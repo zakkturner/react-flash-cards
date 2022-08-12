f5ed60bfa4ed53efdf1618fd376632854867c237
Zak Tee <zakkturner@yahoo.com>
2022-08-12 08:30:35 -0400

    added card header going still need to add navigation and confidence features

--- src/pages/carddetails/CardDetail.js
+++ src/pages/carddetails/CardDetail.js
@@ -7,14 +7,44 @@ const CardDetailPage = ({ questions }) => {
   const { questionId } = useParams();
   // finds the matching id in the questions array prop
   const question = questions.find((question) => question.id == questionId);
+  // Card flip logic
   const [isFlipped, setIsFlipped] = useState(false);
   const cardFlipHandler = (e) => {
     e.preventDefault();
     setIsFlipped(!isFlipped);
   };
+  // temporary will maybe think about using state management for this
+  const levelColor = (level) => {
+    switch (level) {
+      case 0:
+        return "#9D2235";
+
+      case 1:
+        return "#AC9F3C";
+      case 2:
+        return "#006E33";
+      default:
+        return "gray";
+    }
+  };
   return (
     <div className={style.cardDetailPage}>
-      <div class={style.cardDetail_container}>
+      <div
+        class={style.cardDetail_container}
+        // style={{ backgroundColor: isFlipped ? "#61dafb" : "white" }}
+      >
+        <div
+          className={style.cardDetail_header}
+          style={
+            isFlipped
+              ? { backgroundColor: levelColor(question.level), color: "white" }
+              : { backgroundColor: "#61dafb", color: "black" }
+          }
+        >
+          <p className={style.cardDetail_questionNum}>
+            Question Number {question.id}
+          </p>
+        </div>
         <div className={style.cardDetail_text_container}>
           <h1 className={style.cardDetail_text}>
             {!isFlipped ? question.question : question.answer}
